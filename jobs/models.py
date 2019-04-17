33bfb38cef7a439c882555ff3ce4b0a67009ff7d
Andi Sugandi <andisugandi@gmail.com>
2019-04-17 22:42:18 +0700

    Add Job models and database, get object data by App views, and let template displays the data from it

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
